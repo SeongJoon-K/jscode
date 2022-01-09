5138515c3a727a63a16f3dab3fd7eb80125bd14f
hisocean <jobcho6320@naver.com>
2022-01-09 17:19:43 +0900

    jspratice.1

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log('Hello code')
-console.log(10 - 5)
+// 변수 선언
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 4800;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice * 4 + mochaPrice * 2);
+console.log(lattePrice + mochaPrice);
+console.log(espressoPrice + lattePrice * 4 + mochaPrice * 2);
+console.log(lattePrice * 3);
+
+// /* 목적을 명확히
+// 불필요한 것들은 숨기고
+// 핵심만 */
+
+
+
+console.log(espressoPrice)
