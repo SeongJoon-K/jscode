d7fd2516578eca355fe4772d7c537e1c8339e4a7
hisocean <jobcho6320@naver.com>
2022-01-12 17:00:55 +0900

    112js2

--- index110.js
+++ index110.js
@@ -1,9 +1,70 @@
 
-// 문자열 
-console.log("He said \"I'm Iron man\"");
-console.log("He said \"I'm Iron man\"");
-console.log("He said \"I'm Iron man\"");
+// let temperature = 1;
 
-// 위 같은 경우에는 문자열을 읽기가 너무 불편해서 빽 틱 ``을 사용한다.
+// if (temperature <= 0) {
+//     console.log('물이 업니다.')
+// } else {
+//     console.log('물이 얼지 않습니다.');
+// }
 
-console.log(`He said "I'm Iron man"`)
+// function printGrade(midtermScore, finalScore){
+// 	let totalScore = midtermScore + finalScore;
+//   if (totalScore >= 90) {
+//     console.log("A");
+//   } else if (totalScore >= 80) {
+//     console.log("B");
+//   } else if (totalScore >= 70) {
+//     console.log("C");
+//   } else if (totalScore >= 60) {
+//     console.log("D");
+//   } else {
+//     console.log("F")
+//   }
+// }
+// // 테스트 코드
+// printGrade(25, 35);
+// printGrade(50, 45);
+// printGrade(29, 24);
+// printGrade(37, 42);
+
+// 나의 나이와, 나의 성별을 저장하는 변수
+let myAge = 26;
+let myGender = 'male';
+
+// 호칭을 담은 변수
+let callOlderBrother = '형';
+let callOlderSister = '누나';
+let callFriend = '친구';
+let callYoungerSister = '여동생';
+let callYoungerBrother = '남동생';
+
+// 상대방의 나이와 성별에 따른 호칭을 리턴하는 함수 whatShouldICall를 완성하세요.
+function whatShouldICallYou(yourAge, yourGender) {
+  if (myAge === yourAge) {
+    return callFriend;
+  } else if (myAge > yourAge) {
+    if (yourGender === 'male') {
+      return callYoungerBrother;
+    } else {
+      return callYoungerSister;
+  } else {
+      if (yourGender === 'male') {
+          return callOlderBrother;
+      } else if (yourGender === 'female') {
+          return callOlderSister;
+      }
+  }
+}
+
+// 테스트 코드
+let result1 = whatShouldICallYou(25, 'female');
+let result2 = whatShouldICallYou(20, 'male');
+let result3 = whatShouldICallYou(26, 'female');
+let result4 = whatShouldICallYou(30, 'male');
+let result5 = whatShouldICallYou(31, 'female');
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
